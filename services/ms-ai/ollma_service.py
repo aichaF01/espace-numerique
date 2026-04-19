19f071ecc9b42e51e4439d233aab9fcd31680c81
issaouiyasmine <issaouiyasmine@gmail.com>
2026-04-19 14:40:45 +0100

    ollama enpoint fix

--- services/ms-ai/ollma_service.py
+++ services/ms-ai/ollma_service.py
@@ -1,10 +1,10 @@
 import requests 
 
-OLLAMA_URL = "http://localhost:11434/api/generate"
+OLLAMA_URL = "http://ollama:11434/api"
 
 def generate_response(prompt: str, model: str):
     response = requests.post(
-        OLLAMA_URL,
+        f"{OLLAMA_URL}/generate",
         json={
             "model": model,
             "prompt": prompt,
@@ -20,5 +20,7 @@ def generate_response(prompt: str, model: str):
 
 
 def get_models():
-    response = requests.get("http://localhost:11434/api/tags")
+    response = requests.get(f"{OLLAMA_URL}/tags")
+    if response.status_code != 200:
+        raise Exception(f"Ollama error: {response.text}")
     return response.json()
