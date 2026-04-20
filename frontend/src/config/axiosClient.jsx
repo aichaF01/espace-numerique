0154c4637ebd79d47d5925fb0107b41bbdf75ab1
aicha fikry <fikryaicha99@gmail.com>
2026-04-20 16:07:35 +0100

    .

--- frontend/src/config/axiosClient.jsx
+++ frontend/src/config/axiosClient.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
+  // baseURL: 'http://192.168.1.207',
   baseURL: 'http://localhost',
   headers : {
     'Content-Type' : 'application/json',
